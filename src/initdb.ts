40eb65151c4c0969d80d442b35626693d03d8a56
SpaceMonkeyJT <spacemonkeyjt1@gmail.com>
2024-12-12 11:42:34 +0800

    Add script to create initial db

---
+++ src/initdb.ts
@@ -0,0 +1,20 @@
+import * as db from './db';
+
+async function main() {
+  await db.client.connect();
+
+  await db.client.query(`
+    CREATE TABLE IF NOT EXISTS tasks (
+      id SERIAL PRIMARY KEY,
+      server TEXT NOT NULL,
+      username TEXT NOT NULL,
+      name TEXT NOT NULL,
+      active BOOLEAN NOT NULL DEFAULT FALSE,
+      completed BOOLEAN NOT NULL DEFAULT FALSE
+    )
+  `);
+
+  process.exit(0);
+}
+
+main().catch(console.error);
